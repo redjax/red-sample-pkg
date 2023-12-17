7868619d7108975f59b469d2b77d3f2b2b26a2e8
redjax <jackenyon@gmail.com>
2023-12-17 19:32:59 +0000

    Add basic hello_world functionality

---
+++ src/sample_pkg/__init__.py
@@ -0,0 +1,5 @@
+import sys
+
+sys.path.append(".")
+
+from .main import say_hello
---
+++ src/sample_pkg/__main__.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.path.append(".")
+
+from sample_pkg import say_hello
+
+if __name__ == "__main__":
+    print(say_hello())
